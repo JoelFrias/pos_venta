67c3db767645040180c82886ed4e2de1d1ea334f
JoelFrias <fjoelfrias@gmail.com>
2025-01-23 22:58:15 -0400

    prueba

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php 
-
-    echo $_GET['url'];
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    puto
+</body>
+</html>
